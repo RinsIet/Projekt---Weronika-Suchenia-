0ff08ba50e5e9c61a6a36bec025b31bf765ec4e4
Rinslet <nikasuchenia@gmail.com>
2017-12-16 12:06:12 +0100

    teleportacja i blender

---
+++ Assets/scripts/teleporter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleporter : MonoBehaviour {
+	public GameObject destination;
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.CompareTag ("Player") || collider.gameObject.layer == 1)
+		{
+
+			Vector3 targetPosition = destination.transform.position;
+			targetPosition += destination.transform.forward * 2;
+			collider.transform.position = targetPosition;
+
+			destination.GetComponent<AudioSource> ().Play ();
+		}
+
+	}
+									
+
+}
